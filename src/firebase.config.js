fb9ab810a20aacf2e435ab28f14128b5590ad953
Pranav Gupta <119832460+pranavgupta022@users.noreply.github.com>
2024-07-01 16:15:01 +0530

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBZp9OVv2WyyxrS6IhUej_RgpEwBBesCXE",
-  authDomain: "otp-project-1c7a2.firebaseapp.com",
-  projectId: "otp-project-1c7a2",
-  storageBucket: "otp-project-1c7a2.appspot.com",
-  messagingSenderId: "1057684251986",
-  appId: "1:1057684251986:web:7af946a38015fc6458068b",
-  measurementId: "G-1HP5P3BZBJ",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
